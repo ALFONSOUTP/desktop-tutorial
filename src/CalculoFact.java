949dfd940f346f6c7df2e3f029cb63d923b683d5
ALFONSO VASQUEZ LUDEÑA <94215451+ALFONSOUTP@users.noreply.github.com>
2023-06-17 20:55:25 -0500

    Fatorial de un numero

---
+++ src/CalculoFact.java
@@ -0,0 +1,10 @@
+public class CalculoFact {
+	public static void main (String [] a)
+	{
+		
+		final int M = 15;
+		for (int n =1; n<=M; n++)
+			System.out.println(n + "! =" + Matem.factorial(n));
+		
+	}
+}
